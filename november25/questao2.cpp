d69be02c2d0b81315db1127e4b4d1c381bd3ab39
Jojo <mjoao8219@gmail.com>
2025-11-25 16:25:29 -0300

    vaiLOGO

---
+++ november25/questao2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main(){
+    float salario, prestacao;
+    std :: cout << "Digite o seu salario e a prestacao\n ";
+    std :: cin >> salario >> prestacao;
+    if (prestacao < (salario/5))
+        std :: cout << "Emprestimo aceito \n";
+
+}
