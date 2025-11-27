91a045cd3818c54ca1c692fca0825ff40a53d042
starCoder995 <mjoao8219@gmail.com>
2025-11-27 15:42:36 -0300

    feito

--- november25/TesteDeArquivos.cpp
+++ november25/TesteDeArquivos.cpp
@@ -8,9 +8,10 @@ int main(){
     int i = 0;
     const fs :: path pasta = fs :: current_path();
     string listaArquivos[5];
+    cout << "Veja os arquivos na pasta \n";
     for (auto const& iteradorDePasta : fs :: directory_iterator{pasta}){
         listaArquivos[i] = iteradorDePasta.path().string();
-        cout << listaArquivos[i];
+        cout << listaArquivos[i] << "\n";
         i++;
     }
 
